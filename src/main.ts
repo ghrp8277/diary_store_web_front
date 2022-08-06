d7e27273848d00c6945fb3638e7bf8c212c0180a
ghrp8277 <105510128+ghrp8277@users.noreply.github.com>
2022-08-06 23:24:50 +0900

    package setting
    vue composition api
    pinia
    package upload

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueCompositionAPI from "@vue/composition-api";
+import { createPinia, PiniaVuePlugin } from "pinia";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionAPI);
+
+Vue.use(PiniaVuePlugin);
+
+const pinia = createPinia();
 
 new Vue({
   router,
   store,
+  pinia,
   render: (h) => h(App),
 }).$mount("#app");
